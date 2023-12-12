5cd58a1290db6315809b7a5fa7f7273f9fb449b4
Deepen Dhamecha <deepen@upstreamgs.com>
2023-12-12 14:29:16 +0530

    Only Image

---
+++ README.md
@@ -0,0 +1,8 @@
+# Meals App
+
+1. Setting Global fonts
+2. Grid Layout with GridView
+3. Gradient Background
+4. Make Container tappable using Inkwell. You can also use GestureDetector. There are lot of options and not just tappable
+5. Switch screens using Stacking Screens
+6. 
---
+++ lib/screens/meals.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/widgets/meal_item.dart';
+
+class MealsScreen extends StatelessWidget {
+
+  const MealsScreen({
+    super.key,
+    required this.title,
+    required this.meals
+  });
+
+  final String title;
+  final List<Meal> meals;
+
+  @override
+  Widget build(BuildContext context) {
+
+    Widget content;
+
+    if(meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => MealItem(meal: meals[index])
+      );
+    } else {
+      content = Center(child: Column(mainAxisSize: MainAxisSize.min, children: [
+        Text('Uh oh... nothing here!', style: Theme.of(context).textTheme.bodyLarge!.copyWith(color: Theme.of(context).colorScheme.onBackground)),
+        const SizedBox(height: 16),
+        Text('Try selecting a different category!', style: Theme.of(context).textTheme.bodyLarge!.copyWith(color: Theme.of(context).colorScheme.onBackground),)
+      ],),);
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: content,
+    );
+  }
+
+  // children: [
+  //         for(final m in meals)
+  //           MealItem(meal: m)
+  //       ]
+
+}
